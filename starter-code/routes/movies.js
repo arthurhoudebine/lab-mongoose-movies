032df4bae8f81256e0dba52b79e849c70530795e
Arthur Houdebine <arthur.houdebine@gmail.com>
2019-01-05 15:12:37 +0100

    added the option to add a celebrity to a movie

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const Movie = require('../models/movie.js');
+const Celebrity = require('../models/celebrity.js');
+
 
 
 router.get('/movies', (req, res, next) => {
@@ -33,7 +35,7 @@ router.post('/movies/new', (req, res, next) => {
 
   router.get('/movies/:id', (req, res, next) => {
     let movieId = req.params.id;
-    Movie.findOne({'_id': movieId})
+    Movie.findOne({'_id': movieId}).populate('celebrities')
       .then(movie => {
         res.render("movies/show", { movie });
       })
@@ -75,6 +77,29 @@ router.post('/movies/new', (req, res, next) => {
           console.log(error);
         })
       });
+
+      router.get('/movies/addcelebrity/:id', (req, res, next) => {
+        let movId = req.params.id;
+    Movie.findOne({'_id': movId})
+          .then(movie => {
+            res.render("movies/addcelebrity", { movie });
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        });
+        
+        router.post('/movies/addcelebrity/:id', (req, res, next) => {
+          const { name, occupation, catchPhrase } = req.body;
+          const newCeleb = new Celebrity({name, occupation, catchPhrase})          
+          newCeleb.save(function (err, celeb) {
+            if (err) return handleError(err);
+            Movie.update({_id: req.params.id}, { $push: {celebrities: celeb._id}}).then(()=>{
+              res.redirect('/movies/'+req.params.id);  
+            })
+            });
+          });
+    
   
 
 module.exports = router;
